44ae29e061c93ed13115f403fbc814ad728544af
mathewdunne <73080543+mathewdunne@users.noreply.github.com>
2026-03-16 14:55:06 -0400

    more setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { ThemeProvider } from "./components/theme/theme-provider.tsx";
 
 // biome-ignore lint/style/noNonNullAssertion: Root element is guaranteed to be present in the HTML
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
